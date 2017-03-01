48c482893ffbe2fa9335769e23bd0a590f68b23b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-03-01 18:39:19 +0700

    Fix invalid function name, add new function to index.

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ exports.filter = require('./lib/filter');
 exports.addWords = require('./lib/add-words');
 exports.replaceWords = require('./lib/replace-words');
 exports.badWords = require('./lib/bad-words');
+exports.addWhitelist = require('./lib/add-whitelist');
+exports.replaceWhitelist = require('./lib/replace-whitelist');
